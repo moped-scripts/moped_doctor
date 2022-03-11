b81ca3156bf85db6fd7698d517fcf432c648f96b
totsu <71902025+totsu0jv@users.noreply.github.com>
2022-03-11 23:02:48 +0100

    making the standalone option the default one

--- configS.lua
+++ configS.lua
@@ -3,4 +3,4 @@ Config = {}
 Config.Framework = 'vorp' -- redemrp, vorp. To use your own framework look into the 'moped_doctor:checkin' server event or just use Config.Standalone 
 Config.Money = 1 -- How many dollars should it cost to get patched up?
 
-Config.Standalone = false -- a patch up from the doctor will be free - no framework needed for the whole script. -- CAN ALSO USE IF YOU'RE USING VORP/REDEMRP BUT WANT PEOPLE TO USE DOCTOR FOR FREE
+Config.Standalone = true -- a patch up from the doctor will be free - no framework needed for the whole script. -- CAN ALSO USE IF YOU'RE USING VORP/REDEMRP BUT WANT PEOPLE TO USE DOCTOR FOR FREE
